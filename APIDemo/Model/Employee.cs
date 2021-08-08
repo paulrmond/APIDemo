be8fad67e971d54e994356ee107c18908495a794
vdrprbalena <email@ex.com>
2021-08-08 20:37:52 +0800

    first commit

---
+++ APIDemo/Model/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIDemo.Model
+{
+    public class Employee
+    {
+        public string EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeAddress { get; set; }
+    }
+}
